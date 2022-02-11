cfc9d11b088a7dc5fcb1a0971a310e4a62d734e5
Anuj Parikh <anujparikh1@gmail.com>
2022-02-11 16:26:17 -0600

    Permutation in String

---
+++ src/permutation-in-string.ts
@@ -0,0 +1,33 @@
+// @ts-nocheck
+/**
+ * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+ * In other words, return true if one of s1's permutations is the substring of s2.
+ *
+ * Input: s1 = "ab", s2 = "eidbaooo"
+ * Output: true
+ * Explanation: s2 contains one permutation of s1 ("ba").
+ *
+ * Input: s1 = "ab", s2 = "eidboaoo"
+ * Output: false
+ */
+export default (s1: string, s2: string): boolean => {
+  if (s1.length > s2.length) return false;
+  const charMappingArray = Array(26).fill(0);
+  for (let i = 0; i < s1.length; i++) {
+    charMappingArray[s1.charCodeAt(i) - 97]++;
+  }
+  for (let i = 0; i < s2.length; i++) {
+    const subString = s2.substring(i, i + s1.length);
+    const subStringCharMappingArray = Array(26).fill(0);
+    for (let j = 0; j < subString.length; j++) {
+      subStringCharMappingArray[subString.charCodeAt(j) - 97]++;
+    }
+    if (
+      JSON.stringify(charMappingArray) ===
+      JSON.stringify(subStringCharMappingArray)
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
