c9da62b918a677f1d8c9b9d4d78b16dec3064c4b
Anuj Parikh <anujparikh1@gmail.com>
2022-02-14 18:15:01 -0600

    Search Insert Position

---
+++ src/__tests__/search-insert-position.ts
@@ -0,0 +1,13 @@
+import searchInsertPosition from "../search-insert-position";
+
+describe(" Search Insert Position", () => {
+  it("simple input", () => {
+    expect(searchInsertPosition([1, 3, 5, 6], 5)).toBe(2);
+    expect(searchInsertPosition([1, 3, 5, 6], 2)).toBe(1);
+    expect(searchInsertPosition([1, 3, 5, 6], 7)).toBe(4);
+  });
+
+  it("negative value inputs", () => {
+    expect(searchInsertPosition([-10, -7, -6, -4, 2, 3, 5], 4)).toBe(6);
+  });
+});
