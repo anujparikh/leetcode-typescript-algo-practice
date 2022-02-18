3577eb932a0512e4b367abec1145dd3fc8a7660b
Anuj Parikh <anujparikh1@gmail.com>
2022-02-17 19:06:29 -0600

    Add Binary

---
+++ src/__tests__/add-binary.ts
@@ -0,0 +1,9 @@
+import addBinary from "../add-binary";
+
+describe(" Add Binary", () => {
+  it("simple input", () => {
+    expect(addBinary("100", "100")).toBe("1000");
+    expect(addBinary("100", "1")).toBe("101");
+    expect(addBinary("1010", "1011")).toBe("10101");
+  });
+});
---
+++ src/add-binary.ts
@@ -0,0 +1,16 @@
+export default (a: string, b: string): string => {
+  let output = "";
+  let carry = 0;
+
+  for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; i--, j--) {
+    let valA: number = a[i] ? a.charCodeAt(i) - "0".charCodeAt(0) : 0;
+    let valB: number = b[j] ? b.charCodeAt(j) - "0".charCodeAt(0) : 0;
+    const sum = valA + valB + carry;
+    output = (sum % 2) + output;
+    carry = ~~(sum / 2);
+  }
+  if (carry > 0) {
+    output = "1" + output;
+  }
+  return output;
+};
