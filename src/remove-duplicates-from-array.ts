3ea8a209744a07d28bf93dd3fb9ba27c7bc612fb
Anuj Parikh <anujparikh1@gmail.com>
2022-03-10 23:45:11 -0600

    remove duplicates from sorted array

--- src/remove-duplicates-from-array.ts
+++ src/remove-duplicates-from-array.ts
@@ -2,14 +2,11 @@ export default (nums: number[]): number => {
   if (nums.length === 1) return 1;
   let updatePointer = 0;
   let currentPointer = 0;
-  let previousValue = undefined;
   while (currentPointer < nums.length) {
-    if (nums[currentPointer] !== previousValue) {
-      nums[updatePointer] = nums[currentPointer];
-      previousValue = nums[currentPointer];
-      updatePointer++;
+    if (nums[currentPointer] !== nums[updatePointer]) {
+      nums[++updatePointer] = nums[currentPointer];
     }
     currentPointer++;
   }
-  return updatePointer;
+  return updatePointer + 1;
 };
