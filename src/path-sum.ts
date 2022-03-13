4883eaf984cbd56f792a4de560cfd9f0cc9566c1
Anuj Parikh <anujparikh1@gmail.com>
2022-03-13 17:42:45 -0500

    sum of root-to-leaf is equal to target sum

---
+++ src/path-sum.ts
@@ -0,0 +1,20 @@
+import { TreeNode } from "./utils";
+
+/**
+ * https://leetcode.com/problems/path-sum/
+ */
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (root === null) return false;
+  if (targetSum - root.val === 0 && root.left === null && root.right === null)
+    return true;
+  if (root.left !== null && root.right !== null) {
+    return (
+      hasPathSum(root.left, targetSum - root.val) ||
+      hasPathSum(root.right, targetSum - root.val)
+    );
+  }
+  return (
+    hasPathSum(root.left, targetSum - root.val) ||
+    hasPathSum(root.right, targetSum - root.val)
+  );
+}
