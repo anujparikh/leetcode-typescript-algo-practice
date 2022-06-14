d84b2b8e62aabe921744e7a47cb2316ba45b8048
anuj parikh <anujparikh1@gmail.com>
2022-06-14 17:34:38 -0500

    Kth smallest element in M sorted arrays

---
+++ src/kth-smallest-number-m-arr.js
@@ -0,0 +1,17 @@
+const Heap = require('./collections/heap'); //http://www.collectionsjs.com
+
+const find_Kth_smallest = function (lists, k) {
+    const minHeap = new Heap(null, null, (a, b) => b[0] - a[0]);
+    for (let i = 0; i < lists.length; i++) {
+        minHeap.push([lists[i][0], 0, lists[i]]);
+    }
+    while (minHeap.length > 0) {
+        k--;
+        const [current, index, list] = minHeap.pop();
+        if (k === 0) return current;
+        if (list.length > index) {
+            minHeap.push([list[index + 1], index + 1, list])
+        }
+    }
+    return -1;
+};
