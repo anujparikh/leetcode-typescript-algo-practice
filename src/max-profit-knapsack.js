baf38ae9f1558b144f405918193fee8284cdc152
Anuj Parikh <anujparikh1@gmail.com>
2022-06-17 19:30:37 -0500

    max profit for given weights and capacity

---
+++ src/max-profit-knapsack.js
@@ -0,0 +1,33 @@
+let solveKnapsack = function (profits, weights, capacity) {
+  let knapsackRecursive = (profits, weights, capacity, currentIndex) => {
+    if (capacity <= 0 || currentIndex >= weights.length) return 0;
+    let profitWithItem = 0;
+    if (weights[currentIndex] <= capacity) {
+      profitWithItem =
+        profits[currentIndex] +
+        knapsackRecursive(
+          profits,
+          weights,
+          capacity - weights[currentIndex],
+          currentIndex + 1
+        );
+    }
+    const profitWithoutItem = knapsackRecursive(
+      profits,
+      weights,
+      capacity,
+      currentIndex + 1
+    );
+    return Math.max(profitWithItem, profitWithoutItem);
+  };
+  return knapsackRecursive(profits, weights, capacity, 0);
+};
+
+var profits = [1, 6, 10, 16];
+var weights = [1, 2, 3, 5];
+console.log(
+  `Total knapsack profit: ---> ${solveKnapsack(profits, weights, 7)}`
+);
+console.log(
+  `Total knapsack profit: ---> ${solveKnapsack(profits, weights, 6)}`
+);
