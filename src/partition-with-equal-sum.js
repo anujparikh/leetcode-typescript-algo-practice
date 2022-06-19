86b57e5359fcb57bd3187a1b3c3ac12eb3dff7d0
anuj parikh <anujparikh1@gmail.com>
2022-06-19 14:35:35 -0500

    partition with equal sum (dynamic programming)

--- src/partition-with-equal-sum.js
+++ src/partition-with-equal-sum.js
@@ -12,3 +12,23 @@ const partitionWithEqualSum = (nums) => {
     if (totalSum % 2 !== 0) return false;
     return pairWithSum(nums, totalSum/2, 0);
 }
+
+const partitionWithEqualSumDP = (nums) => {
+    const dp = [];
+    const pairWithSum = (nums, sum, currentIndex) => {
+        dp[currentIndex] = dp[currentIndex] || [];
+        if (sum === 0) return true;
+        if (nums.length === 0 || currentIndex >= nums.length) return false;
+        if (dp[currentIndex] && !dp[currentIndex][sum]) {
+            if (nums[currentIndex] <= sum && pairWithSum(nums, sum - nums[currentIndex], currentIndex + 1)) {
+                dp[currentIndex][sum] = true;
+                return true;
+            }
+            dp[currentIndex][sum] = pairWithSum(nums, sum, currentIndex + 1);
+        }
+        return dp[currentIndex][sum];
+    }
+    const totalSum = nums.reduce((previous, current) => current + previous, 0);
+    if (totalSum % 2 !== 0) return false;
+    return pairWithSum(nums, totalSum/2, 0);
+}
