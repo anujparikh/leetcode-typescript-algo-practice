82588dc439956aa168fb68c3a02c72246b1fc5df
Anuj Parikh <anujparikh1@gmail.com>
2022-06-25 18:39:54 -0500

    Count of Subset Sum (DP Bottom up)

--- src/no-of-subset-with-sum.js
+++ src/no-of-subset-with-sum.js
@@ -17,3 +17,26 @@ const noOfSubsetsWithSumBruteForce = (num, sum) => {
 
   return canPartition(0, sum);
 };
+
+const noOfSubsetsWithSumDP = (num, sum) => {
+  const n = num.length;
+  const dp = Array(n)
+    .fill(0)
+    .map(() => Array(sum + 1).fill(0));
+
+  for (let i = 0; i < n; i++) dp[i][0] = 1;
+  for (let s = 1; s <= sum; s++) dp[0][s] = num[0] === s ? 1 : 0;
+
+  for (let i = 1; i < n; i++) {
+    for (let s = 1; s <= sum; s++) {
+      // exclude the number
+      dp[i][s] = dp[i - 1][s];
+      // include the number, if it does not exceed the sum
+      if (s >= num[i]) {
+        dp[i][s] += dp[i - 1][s - num[i]];
+      }
+    }
+  }
+
+  return dp[n - 1][sum];
+};
