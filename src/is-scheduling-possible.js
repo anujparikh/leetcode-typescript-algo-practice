20587ec7ee4ed66fb0aa9d1acb97ab2af2585dbf
Anuj Parikh <anujparikh1@gmail.com>
2022-07-16 14:33:01 -0500

    Is scheduling of task possible

---
+++ src/is-scheduling-possible.js
@@ -0,0 +1,43 @@
+const Deque = require('./collections/deque'); //http://www.collectionsjs.com
+
+const is_scheduling_possible = function (tasks, prerequisites) {
+    const sortedOrder = [];
+    if (tasks <= 1) return true;
+    const inDegree = new Array(tasks).fill(0);
+    const graph = new Array(tasks).fill(0).map(() => new Array())
+
+    prerequisites.forEach((prereq) => {
+        const [parent, child] = prereq;
+        graph[parent].push(child);
+        inDegree[child]++;
+    })
+
+    const sources = new Deque();
+    for (let i = 0; i < inDegree.length; i++) {
+        if (inDegree[i] === 0) {
+            sources.push(i);
+        }
+    }
+
+    while (sources.length > 0) {
+        const vertex = sources.shift();
+        sortedOrder.push(vertex);
+        graph[vertex].forEach((child) => { // get the node's children to decrement their in-degrees
+            inDegree[child] -= 1;
+            if (inDegree[child] === 0) {
+                sources.push(child);
+            }
+        });
+    }
+
+    if (sortedOrder.length !== tasks) {
+        return false;
+    }
+
+    return true;
+};
+
+
+console.log(`Is scheduling possible: ${is_scheduling_possible(3, [[0, 1], [1, 2]])}`)
+console.log(`Is scheduling possible: ${is_scheduling_possible(3, [[0, 1], [1, 2], [2, 0]])}`)
+console.log(`Is scheduling possible: ${is_scheduling_possible(6, [[2, 5], [0, 5], [0, 4], [1, 4], [3, 2], [1, 3]])}`)
