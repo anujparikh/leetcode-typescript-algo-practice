4be14196a7415d902c55d2a776b841a8fe469eb9
Anuj Parikh <anujparikh1@gmail.com>
2022-08-07 21:56:05 -0500

    max profit knapsack - bottom up approach

--- src/dynamic-programming/knapsack/max-profit-knapsnak.js
+++ src/dynamic-programming/knapsack/max-profit-knapsnak.js
@@ -27,3 +27,23 @@ const maxProfitMemoization = function (profits, weights, capacity) {
     }
     return maxProfitRecursive(profits, weights, capacity, 0);
 }
+
+const maxProfitBottomUp = function (profits, weights, capacity) {
+    const size = weights.length;
+    if (capacity <= 0 || size === 0 || profits.length != size) return 0;
+    const dp = (new Array(size).fill(0)).map(() => new Array(capacity + 1).fill(0));
+    for (let c = 0; c <= capacity; c++) {
+        if (weights[0] <= c) dp[0][c] = profits[0];
+    }
+    for (let i = 1; i < size; i++) {
+        for (let c = 1; c <= capacity; c++) {
+            let profit1 = 0;
+            if (weights[i] <= c) {
+                profit1 = dp[i - 1][c - weights[i]] + profits[i];
+            }
+            const profit2 = dp[i - 1][c];
+            dp[i][c] = Math.max(profit1, profit2);
+        }
+    }
+    return dp[size - 1][capacity];
+}
