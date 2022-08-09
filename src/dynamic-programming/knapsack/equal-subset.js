9bd30dcdc8b47d3b163c73e94a92a5e3a96a5be2
Anuj Parikh <anujparikh1@gmail.com>
2022-08-08 23:00:13 -0500

    can partition in equal subset (bottom up approach)

--- src/dynamic-programming/knapsack/equal-subset.js
+++ src/dynamic-programming/knapsack/equal-subset.js
@@ -1,5 +1,7 @@
 const canPartitionBruteForce = (nums) => {
-    const halfSum = (nums.reduce((a, b) => a + b, 0)) / 2;
+    const totalSum = nums.reduce((a, b) => a + b, 0);
+    if (totalSum % 2 !== 0) return false;
+    const halfSum = totalSum / 2;
     const canPartition = (nums, sum, index) => {
         if (sum === 0) return true;
         if (sum < 0 || index >= nums.length) return false;
@@ -15,7 +17,9 @@ const canPartitionBruteForce = (nums) => {
 }
 
 const canPartitionMemoization = (nums) => {
-    const halfSum = (nums.reduce((a, b) => a + b, 0)) / 2;
+    const totalSum = nums.reduce((a, b) => a + b, 0);
+    if (totalSum % 2 !== 0) return false;
+    const halfSum = totalSum / 2;
     const dp = [];
     const canPartition = (nums, sum, index) => {
         if (sum === 0) return true;
@@ -32,4 +36,26 @@ const canPartitionMemoization = (nums) => {
         return withNumber || withoutNumber;
     }
     return canPartition(nums, halfSum, 0);
+}
+
+const canPartitionBottomUp = (nums) => {
+    const totalSum = nums.reduce((a, b) => a + b, 0);
+    if (totalSum % 2 !== 0) return false;
+    const halfSum = totalSum / 2;
+    const size = nums.length;
+    const dp = new Array(size).fill(false).map(() => new Array(halfSum + 1).fill(false));
+    for (let i = 0; i < size; i++) dp[i][0] = true;
+    for (let s = 1; s <= halfSum; s++) {
+        dp[0][s] = nums[0] === s;
+    }
+    for (let i = 1; i < size; i++) {
+        for (let s = 1; s <= halfSum; s++) {
+            if (dp[i - 1][s]) {
+                dp[i][s] = dp[i - 1][s];
+            } else if (nums[i] <= s) {
+                dp[i][s] = dp[i - 1][s - nums[i]]
+            }
+        }
+    }
+    return dp[size - 1][halfSum];
 }
