9dda1f027102137310abfe71b0590e5fc0e0b539
Anuj Parikh <anujparikh1@gmail.com>
2022-08-13 17:04:23 -0500

    Count of Subset Sum - memoization

--- src/dynamic-programming/knapsack/count-subset.js
+++ src/dynamic-programming/knapsack/count-subset.js
@@ -14,3 +14,24 @@ const countSubSetBruteForce = (nums, s) => {
     }
     return countSubSet(nums, s, 0);
 }
+
+const countSubSetMemoization = (nums, s) => {
+    const dp = [];
+    const countSubSet = (nums, sum, index) => {
+        dp[index] = dp[index] || [];
+        if (dp[index][sum] !== undefined) return dp[index][sum];
+        if (sum === 0) return 1;
+        if (sum < 0 || index >= nums.length || nums.length === 0) {
+            return 0;
+        }
+        let withNumber = 0;
+        let withoutNumber = 0;
+        if (nums[index] <= sum) {
+            withNumber = countSubSet(nums, sum - nums[index], index + 1);
+        }
+        withoutNumber = countSubSet(nums, sum, index + 1);
+        dp[index][sum] = withNumber + withoutNumber;
+        return dp[index][sum];
+    }
+    return countSubSet(nums, s, 0);
+}
