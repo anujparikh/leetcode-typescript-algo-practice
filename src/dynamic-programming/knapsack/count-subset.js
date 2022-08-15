351eae1acba91c5e69e47487a33c2f9afa74a726
Anuj Parikh <anujparikh1@gmail.com>
2022-08-14 20:54:42 -0500

    Count subset for a sum - bottom up

--- src/dynamic-programming/knapsack/count-subset.js
+++ src/dynamic-programming/knapsack/count-subset.js
@@ -35,3 +35,24 @@ const countSubSetMemoization = (nums, s) => {
     }
     return countSubSet(nums, s, 0);
 }
+
+const countSubSetBottomUp = (nums, sum) => {
+    const size = nums.length;
+    if (size === 0) return 0;
+    if (sum === 0) return 1;
+    const dp = new Array(size).fill(0).map(() => new Array(sum + 1).fill(0));
+    for (let i = 0; i < size; i++) {
+        dp[i][0] = 1;
+    }
+    for (let s = 1; s <= sum; s++) {
+        dp[0][s] = nums[0] === s ? 1 : 0;
+    }
+
+    for (let i = 1; i < size; i++) {
+        for (let s = 1; s <= sum; s++) {
+            dp[i][s] = dp[i - 1][s] + (nums[i] <= s ? dp[i - 1][s - nums[i]] : 0);
+        }
+    }
+
+    return dp[size - 1][sum];
+}
