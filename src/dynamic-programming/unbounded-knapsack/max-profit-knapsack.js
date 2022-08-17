31a8de11615feb62b1e1afcf2ef6032014236697
anuj parikh <anujparikh1@gmail.com>
2022-08-17 14:56:16 -0500

    Max profit knapsack with repeatitive items - Brute force

--- src/dynamic-programming/unbounded-knapsack/max-profit-knapsack.js
+++ src/dynamic-programming/unbounded-knapsack/max-profit-knapsack.js
@@ -1,3 +1,12 @@
 const maxProfitKnapSackBruteForce = (profits, weights, capacity) => {
-
+    const maxProfit = (profits, weights, capacity, index) => {
+        if (capacity <= 0 || index >= weights.length || weights.length === 0) return 0;
+        let withItem = 0;
+        if (weights[index] <= capacity) {
+            withItem = profits[index] + maxProfit(profits, weights, capacity - weights[index], index);
+        }
+        const withoutItem = maxProfit(profits, weights, capacity, index + 1);
+        return Math.max(withItem, withoutItem);
+    }
+    return maxProfit(profits, weights, capacity, 0);
 }
