3f25ce9b62e39a566dfd62bd9109368401f49c9c
anuj parikh <anujparikh1@gmail.com>
2022-08-17 15:07:37 -0500

    Max profit knapsack with repeatitive items - memoization

--- src/dynamic-programming/unbounded-knapsack/max-profit-knapsack.js
+++ src/dynamic-programming/unbounded-knapsack/max-profit-knapsack.js
@@ -10,3 +10,20 @@ const maxProfitKnapSackBruteForce = (profits, weights, capacity) => {
     }
     return maxProfit(profits, weights, capacity, 0);
 }
+
+const maxProfitKnapSackMemoization = (profits, weights, capacity) => {
+    const dp = [];
+    const maxProfit = (profits, weights, capacity, index) => {
+        if (capacity <= 0 || index >= weights.length || weights.length === 0) return 0;
+        dp[index] = dp[index] || [];
+        if (dp[index][capacity] !== undefined) return dp[index][capacity];
+        let withItem = 0;
+        if (weights[index] <= capacity) {
+            withItem = profits[index] + maxProfit(profits, weights, capacity - weights[index], index);
+        }
+        const withoutItem = maxProfit(profits, weights, capacity, index + 1);
+        dp[index][capacity] = Math.max(withItem, withoutItem);
+        return dp[index][capacity]
+    }
+    return maxProfit(profits, weights, capacity, 0);
+}
