c22e216ab67cdbb9d30cd8ccf428a14a0757fcc8
Anuj Parikh <anujparikh1@gmail.com>
2022-08-18 18:31:15 -0500

    total ways to make coin total - memoization

--- src/dynamic-programming/unbounded-knapsack/coin-change.js
+++ src/dynamic-programming/unbounded-knapsack/coin-change.js
@@ -12,3 +12,22 @@ const coinChangeBruteForce = (denominations, total) => {
     }
     return coinChange(denominations, total, 0);
 }
+
+const coinChangeMemoization = (denominations, total) => {
+    const dp = [];
+    const coinChange = (denominations, total, index) => {
+        dp[index] = dp[index] || [];
+        if (dp[index][total] !== undefined) return dp[index][total];
+        if (total === 0) return 1;
+        if (denominations.length === 0 || index >= denominations.length) return 0;
+        let withIndex = 0;
+        let withoutIndex = 0;
+        if (denominations[index] <= total) {
+            withIndex = coinChange(denominations, total - denominations[index], index);
+        }
+        withoutIndex = coinChange(denominations, total, index + 1);
+        dp[index][total] = withIndex + withoutIndex;
+        return dp[index][total]
+    }
+    return coinChange(denominations, total, 0);
+}
