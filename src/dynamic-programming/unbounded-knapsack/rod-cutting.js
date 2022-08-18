674d8eba572f2627914eb9a6b8dbe40ca69ce084
Anuj Parikh <anujparikh1@gmail.com>
2022-08-18 17:38:38 -0500

    Rod Cutting - Memoization approach

--- src/dynamic-programming/unbounded-knapsack/rod-cutting.js
+++ src/dynamic-programming/unbounded-knapsack/rod-cutting.js
@@ -12,3 +12,24 @@ const rodCuttingBruteForce = (lengths, prices, n) => {
     }
     return rodCutting(lengths, prices, n, 0);
 }
+
+const rodCuttingMemoization = (lengths, prices, n) => {
+    const dp = [];
+    const rodCutting = (lengths, prices, n, index) => {
+        if (lengths.length !== prices.length || n <= 0 || index >= lengths.length) {
+            return 0;
+        }
+        dp[index] = dp[index] || [];
+        if (dp[index][n] !== undefined) {
+            return dp[index][n];
+        }
+        let withIndex = 0;
+        if (lengths[index] <= n) {
+            withIndex = prices[index] + rodCutting(lengths, prices, n - lengths[index], index);
+        }
+        const withoutIndex = rodCutting(lengths, prices, n, index + 1);
+        dp[index][n] = Math.max(withIndex, withoutIndex);
+        return dp[index][n];
+    }
+    return rodCutting(lengths, prices, n, 0);
+}
