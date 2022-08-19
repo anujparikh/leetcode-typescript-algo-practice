76e6c32355f06de23ba44f809d46c19bf48cb14f
Anuj Parikh <anujparikh1@gmail.com>
2022-08-19 16:49:33 -0500

    total ways to make coin total - bottom up

--- src/dynamic-programming/unbounded-knapsack/coin-change.js
+++ src/dynamic-programming/unbounded-knapsack/coin-change.js
@@ -31,3 +31,30 @@ const coinChangeMemoization = (denominations, total) => {
     }
     return coinChange(denominations, total, 0);
 }
+
+const coinChangeBottomUp = (denominations, total) => {
+    const size = denominations.length;
+    if (size === 0) return 0;
+    if (total === 0) return 1;
+    const dp = new Array(size).fill(0).map(() => Array(total + 1).fill(0));
+    for (let i = 0; i < size; i++) {
+        dp[i][0] = 1;
+    }
+    for (let t = 1; t <= total; t++) {
+        dp[0][t] = denominations[0] === t ? 1 : 0;
+    }
+    for (let i = 0; i < size; i++) {
+        for (let t = 1; t <= total; t++) {
+            let withIndex = 0;
+            let withoutIndex = 0;
+            if (denominations[i] <= t) {
+                withIndex = dp[i][t - denominations[i]];
+            }
+            if (i > 0) {
+                withoutIndex = dp[i - 1][t];
+            }
+            dp[i][t] = withIndex + withoutIndex;
+        }
+    }
+    return dp[size - 1][total];
+}
