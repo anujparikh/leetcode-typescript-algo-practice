6c9fd52f86a0f9d6adc56190484ec774562d3e85
Anuj Parikh <anujparikh1@gmail.com>
2022-08-26 21:58:17 -0500

    Minimum number of ways to make change: bottom up

--- src/dynamic-programming/unbounded-knapsack/min-coin-change.js
+++ src/dynamic-programming/unbounded-knapsack/min-coin-change.js
@@ -34,3 +34,16 @@ const minCoinChangeMemoization = (denominations, total) => {
     const result = minCoinChange(denominations, total, 0);
     return result === Number.MAX_VALUE ? -1 : result;
 }
+
+const minCoinChangeBottomUp = (denominations, total) => {
+    const size = denominations.length;
+    const dp = new Array(size).fill(Number.MAX_VALUE).map(() => new Array(total + 1).fill(Number.MAX_VALUE));
+    for (let i = 0; i < size; i++) dp[i][0] = 0;
+    for (let i = 0; i < size; i++) {
+        for (let t = 0; t <= total; t++) {
+            if (i > 0) dp[i][t] = dp[i - 1][t];
+            if (denominations[i] <= t) dp[i][t] = Math.min(dp[i][t], dp[i][t - denominations[i]] + 1);
+        }
+    }
+    return dp[size - 1][total] !== Number.MAX_VALUE ? dp[size - 1][total] : -1;
+}
