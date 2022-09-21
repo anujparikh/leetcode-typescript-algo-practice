3870b20252f1eed927e9bf758ba8ea609279f3ff
anuj parikh <anujparikh1@gmail.com>
2022-09-21 16:41:44 -0500

    stair case ways - memoization

--- src/dynamic-programming/fibonacci-numbers/staircase.js
+++ src/dynamic-programming/fibonacci-numbers/staircase.js
@@ -1,11 +1,29 @@
 const staircaseRecursion = (stepNumber) => {
-    if (n === 0) return 1;
-    if (n === 1) return 1;
-    if (n === 2) return 2;
+    if (stepNumber === 0) return 1;
+    if (stepNumber === 1) return 1;
+    if (stepNumber === 2) return 2;
 
     const take1Step = staircaseRecursion(stepNumber - 1);
     const take2Step = staircaseRecursion(stepNumber - 2);
     const take3Step = staircaseRecursion(stepNumber - 3);
 
     return take1Step + take2Step + take3Step;
+}
+
+const staircaseMemoization = (n) => {
+    const dp = [];
+    const staircase = (stepNumber) => {
+        if (n === 0) return 1;
+        if (n === 1) return 1;
+        if (n === 2) return 2;
+
+        if (dp[stepNumber] !== undefined) return dp[stepNumber];
+
+        const take1Step = staircase(stepNumber - 1);
+        const take2Step = staircase(stepNumber - 2);
+        const take3Step = staircase(stepNumber - 3);
+        dp[stepNumber] = take1Step + take2Step + take3Step;
+        return dp[stepNumber];
+    }
+    return staircase(n);
 }
